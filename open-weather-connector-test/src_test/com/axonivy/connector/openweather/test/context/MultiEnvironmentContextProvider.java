845bd3459ad104733c2a4b321acc65eaa5033450
Dinh Nguyen <dinh.nguyentrinhquoc@axonactive.com>
2025-07-31 11:31:02 +0700

    fix typo

--- open-weather-connector-test/src_test/com/axonivy/connector/openweather/test/context/MultiEnvironmentContextProvider.java
+++ open-weather-connector-test/src_test/com/axonivy/connector/openweather/test/context/MultiEnvironmentContextProvider.java
@@ -15,9 +15,9 @@ public class MultiEnvironmentContextProvider implements TestTemplateInvocationCo
 		String testEnv = System.getProperty(OpenWeatherCommonConstants.END_TO_END_TESTING_ENVIRONMENT_KEY);
 		return switch (testEnv) {
 		case OpenWeatherCommonConstants.END_TO_END_TESTING_ENVIRONMENT_VALUE ->
-			Stream.of(new TestEnironmentInvocationContext(OpenWeatherCommonConstants.REAL_CALL_CONTEXT_DISPLAY_NAME));
+			Stream.of(new TestEnvironmentInvocationContext(OpenWeatherCommonConstants.REAL_CALL_CONTEXT_DISPLAY_NAME));
 		default ->
-			Stream.of(new TestEnironmentInvocationContext(OpenWeatherCommonConstants.MOCK_SERVER_CONTEXT_DISPLAY_NAME));
+			Stream.of(new TestEnvironmentInvocationContext(OpenWeatherCommonConstants.MOCK_SERVER_CONTEXT_DISPLAY_NAME));
 		};
 	}
 
