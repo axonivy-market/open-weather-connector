8963274c59380a1888d32fb2d0499a3266a66796
Dinh Nguyen <127725498+ntqdinh-axonivy@users.noreply.github.com>
2025-07-31 14:37:40 +0700

    MARP-2597 new workflow for e2e testing (#58)

--- open-weather-connector-test/src_test/com/axonivy/connector/openweather/test/context/MultiEnvironmentContextProvider.java
+++ open-weather-connector-test/src_test/com/axonivy/connector/openweather/test/context/MultiEnvironmentContextProvider.java
@@ -12,8 +12,13 @@ public class MultiEnvironmentContextProvider implements TestTemplateInvocationCo
 
 	@Override
 	public Stream<TestTemplateInvocationContext> provideTestTemplateInvocationContexts(ExtensionContext context) {
-		return Stream.of(new TestEnironmentInvocationContext(OpenWeatherCommonConstants.REAL_CALL_CONTEXT_DISPLAY_NAME),
-				new TestEnironmentInvocationContext(OpenWeatherCommonConstants.MOCK_SERVER_CONTEXT_DISPLAY_NAME));
+		String testEnv = System.getProperty(OpenWeatherCommonConstants.END_TO_END_TESTING_ENVIRONMENT_KEY);
+		return switch (testEnv) {
+		case OpenWeatherCommonConstants.END_TO_END_TESTING_ENVIRONMENT_VALUE ->
+			Stream.of(new TestEnvironmentInvocationContext(OpenWeatherCommonConstants.REAL_CALL_CONTEXT_DISPLAY_NAME));
+		default ->
+			Stream.of(new TestEnvironmentInvocationContext(OpenWeatherCommonConstants.MOCK_SERVER_CONTEXT_DISPLAY_NAME));
+		};
 	}
 
 	@Override
