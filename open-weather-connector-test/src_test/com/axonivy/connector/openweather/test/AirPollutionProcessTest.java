92ba96f83afdddb00164e68cdaac171295832f9e
Trần Vĩnh Thiện Phúc <143604440+tvtphuc-axonivy@users.noreply.github.com>
2025-08-22 16:34:42 +0700

    Merge pull request #62 from axonivy-market/feature/MARP-2631-Unify-code-for-e2e-testing-approach
    MARP-2631 Remove unused codes

--- open-weather-connector-test/src_test/com/axonivy/connector/openweather/test/AirPollutionProcessTest.java
+++ open-weather-connector-test/src_test/com/axonivy/connector/openweather/test/AirPollutionProcessTest.java
@@ -11,36 +11,22 @@ import java.time.Duration;
 import java.time.OffsetDateTime;
 
 import org.apache.http.HttpStatus;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.TestTemplate;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.api.extension.ExtensionContext;
 import org.openweathermap.api.data2_5.client.AirPollution;
-
-import com.axonivy.connector.openweather.test.context.MultiEnvironmentContextProvider;
-import com.axonivy.connector.openweather.test.utils.OpenWeatherUtils;
 
 import ch.ivyteam.ivy.bpm.engine.client.BpmClient;
 import ch.ivyteam.ivy.bpm.engine.client.ExecutionResult;
 import ch.ivyteam.ivy.bpm.error.BpmError;
-import ch.ivyteam.ivy.bpm.exec.client.IvyProcessTest;
-import ch.ivyteam.ivy.environment.AppFixture;
 
-@IvyProcessTest(enableWebServer = true)
-@ExtendWith(MultiEnvironmentContextProvider.class)
-public class AirPollutionProcessTest {
+public class AirPollutionProcessTest extends BaseSetup {
 	private final Double TEST_LON_VALUE = 40.7484;
 	private final Double TEST_LAT_VALUE = -73.9967;
 
-	@BeforeEach
-	void beforeEach(ExtensionContext context, AppFixture fixture) {
-		OpenWeatherUtils.setUpConfigForContext(context.getDisplayName(), fixture);
-	}
-
 	@TestTemplate
 	void testGetAirPollutionByGeoCode_ReturnsAirPollution(BpmClient client) throws NoSuchFieldException {
-		ExecutionResult result = OpenWeatherUtils.getSubProcessWithNameAndPath(client, GET_AIR_POLLUTION_PROCESS_PATH,
-				GET_AIR_POLLUTION_BY_GEOCODE_SIGNATURE).execute(TEST_LON_VALUE, TEST_LAT_VALUE);
+		ExecutionResult result =
+				getSubProcessWithNameAndPath(client, GET_AIR_POLLUTION_PROCESS_PATH, GET_AIR_POLLUTION_BY_GEOCODE_SIGNATURE)
+						.execute(TEST_LON_VALUE, TEST_LAT_VALUE);
 		var object = result.data().last().get(RESULT_KEY);
 		assertThat(object).isInstanceOf(AirPollution.class);
 	}
@@ -48,8 +34,8 @@ public class AirPollutionProcessTest {
 	@TestTemplate
 	void testGetAirPollutionByGeoCode_ThrowsBpmException(BpmClient client) throws NoSuchFieldException {
 		try {
-			OpenWeatherUtils.getSubProcessWithNameAndPath(client, GET_AIR_POLLUTION_PROCESS_PATH,
-					GET_AIR_POLLUTION_BY_GEOCODE_SIGNATURE).execute(null, null);
+			getSubProcessWithNameAndPath(client, GET_AIR_POLLUTION_PROCESS_PATH, GET_AIR_POLLUTION_BY_GEOCODE_SIGNATURE)
+					.execute(null, null);
 		} catch (BpmError e) {
 			assertThat(e.getHttpStatusCode()).isEqualTo(HttpStatus.SC_BAD_REQUEST);
 		}
@@ -57,7 +43,7 @@ public class AirPollutionProcessTest {
 
 	@TestTemplate
 	void testGetForecastAirPollutionByGeoCode_ReturnsAirPollution(BpmClient client) throws NoSuchFieldException {
-		ExecutionResult result = OpenWeatherUtils.getSubProcessWithNameAndPath(client, GET_AIR_POLLUTION_PROCESS_PATH,
+		ExecutionResult result = getSubProcessWithNameAndPath(client, GET_AIR_POLLUTION_PROCESS_PATH,
 				GET_FORECAST_AIR_POLLUTION_BY_GEOCODE_SIGNATURE).execute(TEST_LON_VALUE, TEST_LAT_VALUE);
 		var object = result.data().last().get(RESULT_KEY);
 		assertThat(object).isInstanceOf(AirPollution.class);
@@ -66,7 +52,7 @@ public class AirPollutionProcessTest {
 	@TestTemplate
 	void testGetForecastAirPollutionByGeoCode_ThrowsBpmException(BpmClient client) throws NoSuchFieldException {
 		try {
-			OpenWeatherUtils.getSubProcessWithNameAndPath(client, GET_AIR_POLLUTION_PROCESS_PATH,
+			getSubProcessWithNameAndPath(client, GET_AIR_POLLUTION_PROCESS_PATH,
 					GET_FORECAST_AIR_POLLUTION_BY_GEOCODE_SIGNATURE).execute(null, null);
 		} catch (BpmError e) {
 			assertThat(e.getHttpStatusCode()).isEqualTo(HttpStatus.SC_BAD_REQUEST);
@@ -77,10 +63,8 @@ public class AirPollutionProcessTest {
 	void testGetHistoricalAirPollutionByGeoCode_ReturnsAirPollution(BpmClient client) throws NoSuchFieldException {
 		OffsetDateTime now = OffsetDateTime.now();
 		OffsetDateTime twoDaysLater = now.plus(Duration.ofDays(2));
-		ExecutionResult result = OpenWeatherUtils
-				.getSubProcessWithNameAndPath(client, GET_AIR_POLLUTION_PROCESS_PATH,
-						GET_HISTORICAL_AIR_POLLUTION_BY_GEOCODE_SIGNATURE)
-				.execute(TEST_LON_VALUE, TEST_LAT_VALUE, now, twoDaysLater);
+		ExecutionResult result = getSubProcessWithNameAndPath(client, GET_AIR_POLLUTION_PROCESS_PATH,
+				GET_HISTORICAL_AIR_POLLUTION_BY_GEOCODE_SIGNATURE).execute(TEST_LON_VALUE, TEST_LAT_VALUE, now, twoDaysLater);
 		var object = result.data().last().get(RESULT_KEY);
 		assertThat(object).isInstanceOf(AirPollution.class);
 	}
@@ -91,7 +75,7 @@ public class AirPollutionProcessTest {
 		OffsetDateTime now = OffsetDateTime.now();
 		OffsetDateTime twoDaysLater = now.plus(Duration.ofDays(2));
 		try {
-			OpenWeatherUtils.getSubProcessWithNameAndPath(client, GET_AIR_POLLUTION_PROCESS_PATH,
+			getSubProcessWithNameAndPath(client, GET_AIR_POLLUTION_PROCESS_PATH,
 					GET_HISTORICAL_AIR_POLLUTION_BY_GEOCODE_SIGNATURE).execute(null, null, now, twoDaysLater);
 		} catch (BpmError e) {
 			assertThat(e.getHttpStatusCode()).isEqualTo(HttpStatus.SC_BAD_REQUEST);
@@ -104,10 +88,8 @@ public class AirPollutionProcessTest {
 		OffsetDateTime now = OffsetDateTime.now();
 		OffsetDateTime twoDaysLater = now.plus(Duration.ofDays(2));
 		try {
-			OpenWeatherUtils
-					.getSubProcessWithNameAndPath(client, GET_AIR_POLLUTION_PROCESS_PATH,
-							GET_HISTORICAL_AIR_POLLUTION_BY_GEOCODE_SIGNATURE)
-					.execute(TEST_LON_VALUE, TEST_LAT_VALUE, twoDaysLater, now);
+			getSubProcessWithNameAndPath(client, GET_AIR_POLLUTION_PROCESS_PATH,
+					GET_HISTORICAL_AIR_POLLUTION_BY_GEOCODE_SIGNATURE).execute(TEST_LON_VALUE, TEST_LAT_VALUE, twoDaysLater, now);
 		} catch (BpmError e) {
 			assertThat(e.getHttpStatusCode()).isEqualTo(HttpStatus.SC_BAD_REQUEST);
 		}
